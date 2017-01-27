dc2a972517cdd7d73b62e29b4c0f4f48031e6456
Devin Beliveau <devinmarieb@gmail.com>
2017-01-27 10:22:19 -0700

    Fix some tests

--- app/components/Header/header.spec.js
+++ app/components/Header/header.spec.js
@@ -5,29 +5,24 @@ import { expect } from 'chai'
 import "isomorphic-fetch"
 
 import Header from './Header'
+import SubHeader from '../SubHeader/SubHeader'
 import Button from '../Button/Button'
-import Input from '../Input/Input'
 
 
 describe('<Header />', function() {
-  it.skip('should display one h1 tags', function() {
-    const wrapper = mount(<Header />)
-    expect(wrapper.find('h1')).to.have.length(1)
+  it('should display one h1 tags', function() {
+    const wrapper = mount(<Header children={ <SubHeader /> }/>)
+    expect(wrapper.find('h1')).to.have.length(3)
   })
 
-  it.skip('should display two Button components', function() {
-    const wrapper = shallow(<Header />)
-    expect(wrapper.find('Button')).to.have.length(2)
+  it('should display two Button components', function() {
+    const wrapper = shallow(<Header children={ <SubHeader /> } />)
+    expect(wrapper.find('Button')).to.have.length(1)
   })
 
-  it.skip('should display one Input component', function() {
-    const wrapper = shallow(<Header />)
-    expect(wrapper.find('Input')).to.have.length(1)
-  })
-
-  it.skip('should have two router links', function() {
-    const wrapper = shallow(<Header />)
-    expect(wrapper.find('Link')).to.have.length(2)
+  it('should have two router links', function() {
+    const wrapper = shallow(<Header children={ <SubHeader /> } />)
+    expect(wrapper.find('Link')).to.have.length(1)
   })
 
 })
